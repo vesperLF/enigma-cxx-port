b1fff80ab77cf264c79718264f8f25218f226215
Dezalk <dezalk.kyakha@gmail.com>
2022-05-06 22:32:23 -0400

    Deleted Java files

--- src/com/mikepound/analysis/fitness/FitnessFunction.java
+++
@@ -1,9 +0,0 @@
-package com.mikepound.analysis.fitness;
-
-public abstract class FitnessFunction {
-    protected final float epsilon = 3e-10f;
-
-    public float score(char[] text) {
-        return 0f;
-    }
-}
